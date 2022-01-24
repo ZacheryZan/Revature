63318a43f96f558bad1ff1cafb531854e56561c6
ZacheryZan <brinner0@gmail.com>
2022-01-24 14:51:20 -0600

    Add files via upload

---
+++ wcc_encstring.py
@@ -0,0 +1,11 @@
+"""Weekly coding challenge"""
+def customStringEncrypt():
+    """encrypts string.
+	step 1. reverse string
+	step 2. replace vowels with new value
+	step 3. add 'aca' to end of string
+	"""
+    s = input().lower()
+    s = s[::-1].replace('a', str(0)).replace('e', str(1)).replace('i', str(2)).replace('o', str(2)).replace('u', str(3))
+    s = s + 'aca'
+    return s
